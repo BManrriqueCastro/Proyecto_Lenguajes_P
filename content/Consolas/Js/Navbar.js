cf085fd0b54572e4f2d81a1d571178e93669582a
BAMC202 <brayan20021005@gmail.com>
2024-09-25 21:00:27 -0500

    add:

--- content/Consolas/Js/Navbar.js
+++
@@ -1,6 +0,0 @@
-function loadNavbar() {
-    fetch("../../../index.html").then(response => response.text()).then(data => {
-        document.getElementById("navbar").innerHTML = data;
-    });
-}
-window.onload = loadNavbar;
